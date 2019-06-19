50e6c43a5a2b38a6693d5366bbd4b6a4e93e5e72
Anthony Agnone <anthonyagnone@Anthonys-MacBook-Pro.local>
2019-06-19 09:27:26 -0400

    Adding in slider

--- pages/pdpRework/assets/js/main.js
+++ pages/pdpRework/assets/js/main.js
@@ -1,5 +1,9 @@
 var breadCrumbLinks = $(".breadCrumbs").find("a");
 
+var price = "30.00";
+
+var formattedPrice = price.split(".");
+
 breadCrumbLinks.each(function(i, el) {
   var chevron = $('<i class="fas fa-chevron-down"></i>');
   if (i !== breadCrumbLinks.length - 1) {
@@ -11,8 +15,28 @@ breadCrumbLinks.each(function(i, el) {
 
 var currentImage = 0;
 
-var altImages = $(".imagesWrap img");
+var altImagesContainer = $(".imagesWrap"),
+  allImages = altImagesContainer.find("img"),
+  imagesCount = allImages.length;
 
-var numAltImages = altImages.length;
+allImages.each(function(i, el) {
+  $(el).data("position", i);
+});
 
-$(".altCount").text(currentImage + 1 + " of " + numAltImages);
+var altCount = $(".altCount");
+
+altCount.text(currentImage + 1 + " of " + imagesCount);
+
+$(altImagesContainer).draggable({
+  axis: "x",
+  containment: [imagesCount * -93, 0, 20, 0],
+  drag: function(event, ui) {
+    currentImage = Math.ceil(ui.position.left / -110);
+    altCount.text(currentImage + 1 + " of " + imagesCount);
+  }
+});
+
+altImagesContainer.css("--n", imagesCount);
+
+$(".price h2").text("$" + formattedPrice[0]);
+$(".price span").text(formattedPrice[1]);
